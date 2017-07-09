3e2792824a0d8f0cbdda45bb03107681ec70eff0
Lancelei <lancelei.h@hotmail.ca>
2017-07-09 05:25:02 -0700

    Page fades in to data

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by Lancelei on 2017-07-09.
  */
+$(document).ready(function(){
+
+    $("#hide").click(function(){
+        $("#introduction").fadeToggle(450);
+
+        setTimeout(function() {
+            $("#main").css("background","#9DC2EA");
+        }, 400);
+        $("#data").fadeToggle(2100);
+    });
+});
